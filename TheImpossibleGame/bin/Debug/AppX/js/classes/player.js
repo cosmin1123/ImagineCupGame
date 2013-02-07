2be5637c71d56b66c37aba102eb19f13d4b5703a
Didii Theodor-Cosmin <cosmin.didii@gmail.com>
2013-02-07 08:55:25 -0800

    Windows 8 works

--- TheImpossibleGame/bin/Debug/AppX/js/classes/player.js
+++ TheImpossibleGame/bin/Debug/AppX/js/classes/player.js
@@ -1,42 +1,42 @@
 function Player (id,width, height,posX,posY) {
-  this.img = document.getElementById(id);
-  this.x = posX;
-  this.y = posY;
-  this.width = width;
-  this.height = height;
-  this.startX = posX;
-  this.startY = posY;
+	this.img = document.getElementById(id);
+	this.x = posX;
+	this.y = posY;
+	this.width = width;
+	this.height = height;
+	this.startX = posX;
+	this.startY = posY;
   
 }
 
 Player.prototype.move = function () {
 	
-	 resizeClear(this.x, this.y, this.width, this.height);
+	resizeClear(this.x, this.y, this.width, this.height);
  
     
-    this.XBefore = this.x;
-    this.YBefore = this.y;
-    if (isRight == true)
-        this.x += 4;
-    if (isLeft == true)
-        this.x -= 4;
-    if (isUp == true)
-        this.y -= 4;
-    if (isDown == true)
-        this.y += 4;
+	this.XBefore = this.x;
+	this.YBefore = this.y;
+	if (isRight == true)
+		this.x += 4;
+	if (isLeft == true)
+		this.x -= 4;
+	if (isUp == true)
+		this.y -= 4;
+	if (isDown == true)
+		this.y += 4;
         
-	 if(isRight || isLeft || isUp || isDown)
-	 	sendingCoords();
+	if(isRight || isLeft || isUp || isDown)
+		sendingCoords();
     
-    wall_collission();
- 	 context.save();
- 	 resizeDraw(this.img, this.x, this.y, this.width, this.height);
+	wallCollision();
+ 	context.save();
+ 	resizeDraw(this.img, this.x, this.y, this.width, this.height);
 
-    context.restore();
+	context.restore();
 
 }
 
-wall_collission = function(){
+wallCollision = function(){
 	
 	for( var i = 0; i < index; i++)	
          if (!(player.x + player.width < walls[i].x ||
